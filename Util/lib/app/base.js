23e37f0ed1956597d8d8035cc00744a1cfec5a4f
YiYing <yiyingqq@gmail.com>
2014-09-24 17:42:55 +0800

    update

--- Util/lib/app/base.js
+++ Util/lib/app/base.js
@@ -7,4 +7,42 @@ define(["jquery","bootstrap"],function($){
         }
       }
     })
+    affixScrollInit();
+    function affixScrollInit(){
+		var $links =  $("#affix ul a");
+		$links.on("click",function(){
+			$(this).parent().addClass("active").siblings(".active").removeClass("active");
+		})
+		$(document).on("scroll",function(){
+			var scrolltop = $(this).scrollTop();
+			if ($(this).height()==window.innerHeight) {
+				return false;
+			};
+			setSidebarPosition(scrolltop);
+			if(scrolltop<=0){
+				$links.first().parent().addClass("active").siblings(".active").removeClass("active");
+				return true;
+			}else if (scrolltop>=($(this).height()-window.innerHeight)) {
+				$links.last().parent().addClass("active").siblings(".active").removeClass("active");
+				return true;
+			}
+			setPosition(scrolltop);
+		})
+	}
+	function setPosition(scrolltop){
+		var $cur = $("#affix .active");
+		if($cur.length==0){return false;}
+		var $target = $("#"+$cur.find("a")[0].href.split("#")[1]);
+		if(scrolltop>$target.offset().top+$target.height()){
+			$cur.removeClass("active").next().addClass("active");
+		}else if (scrolltop<$target.offset().top) {
+			$cur.removeClass("active").prev().addClass("active");
+		}
+	}
+	function getPosition($elems){
+		$elems.each(function(){
+			var $target = $("#"+this.href.split("#")[1]).parent();
+			$(this).data("top",$target.offset().top).data("bottom",$target.offset().top+$target.height());
+		});
+	}
 });
--- Util/lib/util/Util.js
+++ Util/lib/util/Util.js
@@ -11,7 +11,7 @@
         /**
          * 简单模板引擎
          */
-        util.template = (function() {
+        util.template = (function(){
             var cache = {};
             return function(str, data) {
                 // Figure out if we're getting a template, or if we need to
@@ -76,13 +76,59 @@
         };
 
         /**
-         * 模态框
+         * 弹出侧边编辑栏组件
          * @param config
          */
-        util.baseDialog = function(config){
-            require(["baseDialog"],function(baseDialog){
-                baseDialog(config);
-            });
+        util.sidebarDetial = function(config){
+            var param = $.extend({
+                "url":"",
+                "width":"",
+                "allowClick":[]
+            },config);
+
+            require(['text!'+param.url],function(panel){
+                var $Panel = $(panel);
+                //设置弹出面板样式
+                $Panel.css({
+                    "width":param.width,
+                    "right":"-"+param.width,
+                    "border":"1px solid rgba(0,0,0,.2)",
+                    "position":"absolute",
+                    "z-index":"3000",
+                    "top":0,
+                    "padding":"10px",
+                    "background-color":"white",
+                    "overflow-y":"scroll",
+                    "overflow-x":"hidden",
+                    "height":"100%"
+                }).appendTo($(document.body));
+                //弹出侧边编辑栏
+                $Panel.animate({right: 0}, 300);
+                //回调函数执行
+                typeof(param.afterLoad)=="function" && param.afterLoad.apply(this);
+                //添加点击侧边栏之外的元素关闭侧边栏事件监听
+                $(document.body).mouseup(function(e){
+                    //不是目标区域且不是子元素,且不是自定义允许点击节点
+                    if((!$Panel.is(e.target) && $Panel.has(e.target).length === 0) && !isAllowTarget(e)){
+                        //关闭页面
+                        $Panel.animate({right: "-"+param.width}, 300,function(){
+                            $Panel.attr("style","").hide();
+                        });
+                        //取消事件
+                        $(document.body).unbind("mouseup");
+                    }
+                });
+                var isAllowTarget = function(e){
+                    //增加对boostrap date组件的支持
+                    var arr = param.allowClick.concat($('.datetimepicker'));
+                    for(var i= 0,item;item=arr[i++];){
+                        if($(item).is(e.target) || $(item).has(e.target).length){
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+            })
         };
         /***
         * TreeDialog
@@ -124,7 +170,7 @@
                 config.setting.async={
                     enable : true,
                     url : config.data
-                }
+                };
                 config.data=null;
                 config.isCache=false;//异步读取数据时,不缓存ztree
             }
@@ -191,22 +237,13 @@
                 config.tree.setting.async={
                     enable : true,
                     url : config.data
-                }
+                };
                 config.tree.data=null;
                 config.tree.isCache=false;//异步读取数据时,不缓存ztree
             }
             require(["util/dialog/TreeAndGridDialog"],function(treeAndGridDialog){
                 treeAndGridDialog.init(config);
             });
-        }
-        /**
-         * tab页签
-         * @param config
-         */
-        util.tab = function(config){
-
         };
-        
         return util;
-    }
-);
+});
